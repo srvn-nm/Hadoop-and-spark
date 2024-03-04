1e7be6b98244641c63d1ae0be51ace4d503aa0b2
mohamad sadegh mohamadi <msmohamadi1380@gmail.com>
2024-03-04 12:15:27 +0330

    fix build problem

--- master-build.sh
+++ master-build.sh
@@ -5,4 +5,4 @@ cd hadoop/base && docker build . -t hadoop-base && cd ../..
 #Create spark base image
 cd spark/base && docker build . -t spark-base && cd ../..
 #Start all containers
-docker compose up -d
+docker compose up -d --build
