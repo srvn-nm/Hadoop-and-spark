3c0c387c336e0226f02d5bc6aecb934fab3bc45b
Mohammad-Rahmanian <sajadrahmanian1616@gmail.com>
2024-04-28 13:41:49 +0330

    change scripts comments

--- mapper.py
+++ mapper.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python
-"""mapper.py"""
+"""mapper.py - This mapper script reads text input from standard input, in the format "document_id,text".
+It processes each line to extract words and emits each word along with the document ID it appeared in. 
+Each word is emitted only once per document to ensure uniqueness.
+"""
--- reducer.py
+++ reducer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
-"""reducer.py"""
+"""reducer.py - Processes key-value pairs received from the mapper, where each key is a word 
+and each value is a document ID where the word appeared. This script aggregates the document IDs 
+for each word and outputs the word alongside the set of unique document IDs where the word was found.
+The reducer reads lines of input from standard input, where each line has a format of 'word,document_id'.
+The input lines are expected to be sorted by the word. The reducer uses this sorted order to efficiently
+aggregate document IDs by using a set data structure, transitioning between different words as it processes
+the input lines one-by-one.
+"""
